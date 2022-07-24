98e6a695c024d414a6c5f4913c6b0a50c25fc603
Sorin Iordache <jordache_sor@yahoo.com>
2022-07-24 16:53:26 +0300

    Add code to test document classes

--- Electroform Lite/Program.cs
+++ Electroform Lite/Program.cs
@@ -1,5 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Electroform_Lite.Models;
 
-Admin admin = new("admin", "password");
-Console.WriteLine(admin.Name);
+TestDocumentClasses();
+
+void TestDocumentClasses()
+{
+    Document document = new();
+    PDFDocument pdfDocument = new();
+    document.Export();
+    pdfDocument.Export();
+    pdfDocument.Export("watermark");
+}
+
+/*void TestUserClasses()
+{
+    Admin admin = new("admin", "password");
+    Console.WriteLine(admin.Name);
+}*/
