8b73507857034f0173db0b82abb803cb14718ce9
Sorin Iordache <jordache_sor@yahoo.com>
2022-07-24 16:50:14 +0300

    Clean up User and Admin

--- Electroform Lite/Models/Admin.cs
+++ Electroform Lite/Models/Admin.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Electroform_Lite.Models;
 
-namespace Electroform_Lite.Models
+internal class Admin : User
 {
-    internal class Admin : User
+    public Admin(string name, string password) : base(name, password)
     {
-        public Admin(string name, string password) : base(name, password)
-        {
-            
-        }
+        
     }
 }
--- Electroform Lite/Models/User.cs
+++ Electroform Lite/Models/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Electroform_Lite.Models;
+﻿namespace Electroform_Lite.Models;
 
 internal class User
 {
