bc8f3dcae9d2a9305cba67b16b4aff09e1e942d6
Sorin Iordache <jordache_sor@yahoo.com>
2022-07-27 21:59:10 +0300

    Add basic functionality

---
+++ Electroform Lite/Models/GenericArray.cs
@@ -0,0 +1,84 @@
+﻿namespace Electroform_Lite.Models;
+
+internal class GenericArray<T>
+{
+    private readonly int _maxSize;
+    private int _currentIndex;
+    private T[] _items;
+
+    public GenericArray(int maxSize = 10)
+    {
+        _maxSize = maxSize;
+        _items = new T[maxSize];
+        _currentIndex = 0;
+    }
+
+    public T GetItemAt(int index)
+    {
+        return _items[index];
+    }
+
+    public void SetItemAt(int index, T item)
+    {
+        _items[index] = item;
+    }
+
+    public void SwapItems(T item1, T item2)
+    {
+        int index1 = GetIndex(item1);
+        int index2 = GetIndex(item2);
+        SwapItemsAt(index1, index2);
+    }
+
+    public void SwapItemAtWithItem(int item1Index, T item2)
+    {
+        int index2 = GetIndex(item2);
+        SwapItemsAt(item1Index, index2);
+    }
+
+    public void SwapItemWithItemAt(T item1, int item2Index)
+    {
+        int index1 = GetIndex(item1);
+        SwapItemsAt(index1, item2Index);
+    }
+
+    public void SwapItemsAt(int index1, int index2)
+    {
+        (_items[index2], _items[index1]) = (_items[index1], _items[index2]);
+    }
+
+    private int GetIndex(T item)
+    {
+        for (int i = 0; i < _currentIndex; i++)
+        {
+            if (_items[i].Equals(item))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void Push(T item)
+    {
+        if (_currentIndex >= _maxSize)
+        {
+            throw new InvalidOperationException("Stack is full");
+        }
+
+        _items[_currentIndex++] = item;
+    }
+
+    public T Pop()
+    {
+        _currentIndex--;
+
+        if (_currentIndex >= 0)
+        {
+            return _items[_currentIndex];
+        }
+
+        _currentIndex = 0;
+        throw new InvalidOperationException("Stack is empty");
+    }
+}
--- Electroform Lite/Program.cs
+++ Electroform Lite/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Electroform_Lite.Models;
+
+GenericArray<string> genericArray = new(4);
+
+genericArray.Push("a");
+genericArray.Push("b");
+genericArray.Push("c");
+genericArray.Push("d");
+
+Console.WriteLine(genericArray.GetItemAt(0));
+genericArray.SetItemAt(0, "A");
+Console.WriteLine(genericArray.GetItemAt(0));
+
+Console.WriteLine($"arr[0] = {genericArray.GetItemAt(0)}, arr[1] = {genericArray.GetItemAt(1)}");
+//genericArray.SwapItemsAt(0, 1);
+//genericArray.SwapItems("A", "b");
+//genericArray.SwapItemAtWithItem(0, "b");
+genericArray.SwapItemWithItemAt("A", 1);
+Console.WriteLine($"arr[0] = {genericArray.GetItemAt(0)}, arr[1] = {genericArray.GetItemAt(1)}");
