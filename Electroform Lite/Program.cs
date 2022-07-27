b625816153745b3a7c0dc11771a4cc5efe1e4776
Sorin Iordache <jordache_sor@yahoo.com>
2022-07-27 10:36:57 +0300

    Add basic exception

--- Electroform Lite/Program.cs
+++ Electroform Lite/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿TestBasicException();
+
+static void TestBasicException()
+{
+    DateTime date = DateTime.UtcNow.Date;
+    //date = date.AddDays(1);
+    try
+    {
+        ValidateBirthDate(date);
+        Console.WriteLine("Store birth date in database");
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("ERROR: Birth date is in the future");
+    }
+}
+
+static void ValidateBirthDate(DateTime birthDate)
+{
+    try
+    {
+        if (birthDate > DateTime.UtcNow.Date)
+        {
+            throw new Exception();
+        }
+    }
+    catch (Exception)
+    {
+        throw;
+    }
+}
