051c7ad6dc8c90f2ca91963d4b6854708259f126
Sorin Iordache <jordache_sor@yahoo.com>
2022-07-30 10:40:42 +0300

    Generate basic document

--- ElectroformLite.Console/Program.cs
+++ ElectroformLite.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ElectroformLite.Domain.Models;
 using ElectroformLite.Domain.Services;
+using System.Text.RegularExpressions;
 
 List<Data> dataList = DataService.GetData();
 List<DataGroup> dataGroups = DataGroupService.GetDataGroups();
@@ -8,8 +9,70 @@ List<Template> templates = TemplateService.GetTemplates();
 List<Document> documents = DocumentService.GetDocuments();
 
 List<int> dataGroupIndices = new() { 0, 1 };
-List<int> documentIndices = new() { 0 };
+List<int> documentIndices = new();
 
+//User user = UserService.GetUser("John Doh", dataGroupIndices, documentIndices);
 
-User user = UserService.GetUser("John Doh", dataGroupIndices, documentIndices);
-Console.WriteLine($"Hello, {user.Name}!");
+Console.WriteLine("Select template id:");
+try
+{
+    int templateId = int.Parse(Console.ReadLine());
+    Document document = CreateBasicDocument(templateId);
+    Console.WriteLine(document.Name);
+    Console.WriteLine(document.Content);
+}
+catch (Exception)
+{
+    
+}
+
+Document CreateBasicDocument(int templateId)
+{
+    Template template = templates[templateId];
+
+    string output = template.Content;
+
+    List<string> dataGroupTypes = GetDataGroupTypesFromTemplate(template.Content);
+
+    List<int> usedDataGroupIds = new();
+
+    foreach (string dataGroupType in dataGroupTypes)
+    {
+        DataGroup dataGroup = GetDataGroupByType(dataGroupType);
+        usedDataGroupIds.Add(dataGroup.Id);
+
+        foreach (int dataIndex in dataGroup.Data)
+        {
+            Data data = dataList[dataIndex];
+
+            string templatePlaceholder = $"[{dataGroup.Type}.{data.Placeholder}]";
+
+            output = output.Replace(templatePlaceholder, data.Value);
+        }
+    }
+
+    Document document = new()
+    {
+        Name = template.Name.Replace("Template for", "Document:"),
+        TemplateId = templateId,
+        Content = output,
+        Created = DateTime.Now,
+        DataGroups = new(usedDataGroupIds)
+    };
+
+    return document;
+}
+
+List<string> GetDataGroupTypesFromTemplate(string templateContent)
+{
+    MatchCollection matches = Regex.Matches(templateContent, "\\[(.+?)\\.");
+
+    var matchvalues = matches.Select(m => m.Groups[1].Value);
+
+    return matchvalues.Distinct().ToList();
+}
+
+DataGroup GetDataGroupByType(string dataType)
+{
+    return dataGroups.First(g => g.Type == dataType);
+}
