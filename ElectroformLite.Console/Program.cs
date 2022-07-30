bbec98d0e03f582a06d1a07a0bc5178454678c14
Sorin Iordache <jordache_sor@yahoo.com>
2022-07-30 18:19:03 +0300

    Add code to generate class diagram

--- ElectroformLite.Console/Program.cs
+++ ElectroformLite.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ElectroformLite.Domain.Models;
 using ElectroformLite.Domain.Services;
+using System.Text;
 using System.Text.RegularExpressions;
 
 List<Data> dataList = DataService.GetData();
@@ -13,7 +14,7 @@ List<int> documentIndices = new();
 
 //User user = UserService.GetUser("John Doh", dataGroupIndices, documentIndices);
 
-Console.WriteLine("Select template id:");
+/*Console.WriteLine("Select template id:");
 try
 {
     int templateId = int.Parse(Console.ReadLine());
@@ -24,6 +25,76 @@ try
 catch (Exception)
 {
     
+}*/
+
+Console.WriteLine(GenerateClassDiagram()); ;
+
+string GenerateClassDiagram()
+{
+    StringBuilder classDiagramStringBuilder = new();
+    //classDiagramStringBuilder.AppendLine("```mermaid");
+    classDiagramStringBuilder.Append(ExtractMethods(typeof(User)));
+    classDiagramStringBuilder.Append(ExtractMethods(typeof(Data)));
+    classDiagramStringBuilder.Append(ExtractMethods(typeof(DataGroup)));
+    classDiagramStringBuilder.Append(ExtractMethods(typeof(Document)));
+    classDiagramStringBuilder.Append(ExtractMethods(typeof(Template)));
+    //classDiagramStringBuilder.AppendLine("```");
+    return classDiagramStringBuilder.ToString();
+}
+
+StringBuilder ExtractMethods(Type type)
+{
+    List<string> props = new();
+    List<string> methods = new();
+    foreach (var method in type.GetMethods())
+    {
+        var parameters = method.GetParameters();
+        var parameterDescriptions = string.Join(", ", method.GetParameters()
+            .Select(x => x.ParameterType.Name + " " + x.Name)
+            .ToArray());
+        
+        if (type.FullName == method.DeclaringType.FullName)
+        {
+            string methodName = method.Name;
+            if (methodName.StartsWith("get_"))
+            {
+                //props.Add($"{method.ReturnType.Name} {methodName.Replace("get_", "")}");
+                props.Add($"{methodName.Replace("get_", "")}");
+            }
+            else if (methodName.StartsWith("set_"))
+            {
+                //Console.WriteLine($"{method.ReturnType.Name} {methodName.Replace("get_", "")}");
+            }
+            else
+            {
+                //methods.Add($"{method.ReturnType.Name} {methodName}({parameterDescriptions})");
+                methods.Add($"{methodName}({parameterDescriptions})");
+            }
+        }
+    }
+
+    return GenerateClassCode(type.Name, props, methods);
+}
+
+StringBuilder GenerateClassCode(string name, List<string> props, List<string> methods)
+{
+    StringBuilder stringBuilder = new();
+    stringBuilder.AppendLine($"class {name}{{");
+    foreach (string prop in props)
+    {
+        stringBuilder.AppendLine(prop);
+    }
+    if (methods.Count > 0)
+    {
+        stringBuilder.AppendLine();
+    }
+    foreach (string method in methods)
+    {
+        stringBuilder.AppendLine(method);
+    }
+    stringBuilder.AppendLine("}");
+
+    return stringBuilder;
 }
 
 Document CreateBasicDocument(int templateId)
