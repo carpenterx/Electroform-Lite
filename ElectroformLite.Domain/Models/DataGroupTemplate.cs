c3ebe9f42734c3b032ed0b2d87a18898f5188c9b
Sorin Iordache <jordache_sor@yahoo.com>
2022-07-31 11:01:39 +0300

    Add data group template

---
+++ ElectroformLite.Domain/Models/DataGroupTemplate.cs
@@ -0,0 +1,10 @@
+﻿namespace ElectroformLite.Domain.Models;
+
+public class DataGroupTemplate
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public List<int> DataTemplates { get; set; }
+}
