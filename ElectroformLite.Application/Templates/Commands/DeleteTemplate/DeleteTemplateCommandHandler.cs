7b45bb36e3df05e26c69e9c52f7e6b358d906146
Sorin Iordache <jordache_sor@yahoo.com>
2022-08-11 21:35:13 +0300

    Add template delete and edit

---
+++ ElectroformLite.Application/Templates/Commands/DeleteTemplate/DeleteTemplateCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using ElectroformLite.Application.Interfaces;
+using MediatR;
+
+namespace ElectroformLite.Application.Templates.Commands.DeleteTemplate;
+
+public class DeleteTemplateCommandHandler : IRequestHandler<DeleteTemplateCommand>
+{
+    private readonly ITemplateRepository _repository;
+
+    public DeleteTemplateCommandHandler(ITemplateRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Task<Unit> Handle(DeleteTemplateCommand request, CancellationToken cancellationToken)
+    {
+        _repository.Delete(request.TemplateId);
+
+        return Task.FromResult(Unit.Value);
+    }
+}
---
+++ ElectroformLite.Application/Templates/Commands/EditTemplate/EditTemplateCommand.cs
@@ -0,0 +1,14 @@
+﻿using ElectroformLite.Domain.Models;
+using MediatR;
+
+namespace ElectroformLite.Application.Templates.Commands.EditTemplate;
+
+public class EditTemplateCommand : IRequest
+{
+    public Template Template { get; set; }
+
+    public EditTemplateCommand(Template template)
+    {
+        Template = template;
+    }
+}
