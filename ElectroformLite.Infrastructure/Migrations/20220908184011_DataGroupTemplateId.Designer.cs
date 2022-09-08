182f9051f216ac2937375ad30558681c2dbff77d
Sorin Iordache <jordache_sor@yahoo.com>
2022-09-08 21:46:51 +0300

    Add migration

--- ElectroformLite.Infrastructure/Migrations/20220908184011_DataGroupTemplateId.Designer.cs
+++ ElectroformLite.Infrastructure/Migrations/20220908184011_DataGroupTemplateId.Designer.cs
@@ -4,6 +4,7 @@ using ElectroformLite.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ElectroformLite.Infrastructure.Migrations
 {
     [DbContext(typeof(ElectroformDbContext))]
-    partial class ElectroformDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220908184011_DataGroupTemplateId")]
+    partial class DataGroupTemplateId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ElectroformLite.Infrastructure/Migrations/20220908184011_DataGroupTemplateId.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElectroformLite.Infrastructure.Migrations
+{
+    public partial class DataGroupTemplateId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DataGroups_DataGroupTemplates_DataGroupTemplateId",
+                table: "DataGroups");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DataGroupTemplateId",
+                table: "DataGroups",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DataGroups_DataGroupTemplates_DataGroupTemplateId",
+                table: "DataGroups",
+                column: "DataGroupTemplateId",
+                principalTable: "DataGroupTemplates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DataGroups_DataGroupTemplates_DataGroupTemplateId",
+                table: "DataGroups");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DataGroupTemplateId",
+                table: "DataGroups",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DataGroups_DataGroupTemplates_DataGroupTemplateId",
+                table: "DataGroups",
+                column: "DataGroupTemplateId",
+                principalTable: "DataGroupTemplates",
+                principalColumn: "Id");
+        }
+    }
+}
