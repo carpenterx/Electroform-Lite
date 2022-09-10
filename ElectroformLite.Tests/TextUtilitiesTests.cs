88b37cbf217eb4cce4343968f0acd5277f1bcbfa
Sorin Iordache <jordache_sor@yahoo.com>
2022-09-10 16:48:06 +0300

    Refactor tests

--- ElectroformLite.Tests/TextUtilitiesTests.cs
+++ ElectroformLite.Tests/TextUtilitiesTests.cs
@@ -4,22 +4,18 @@ namespace ElectroformLite.Tests;
 
 public class TextUtilitiesTests
 {
-    readonly string value = "Cerere 1 Alocare Credentiale Pentru Plata Impozitelor Si Taxelor Locale Pentru Persoane Fizice\n\nSubsemnatul/a [Person.FirstName] [Person.LastName], e-mail [Contact.Email], numar de\ntelefon [Contact.PhoneNumber], solicit a-mi fi atribuit credential in\nvederea platii prin www.ghiseul.ro";
-    readonly Dictionary<string, string> dataDictionary = new();
-
-    public TextUtilitiesTests()
-    {
-        // Arrange
-        dataDictionary.Add("[Person.FirstName]", "John");
-        dataDictionary.Add("[Person.LastName]", "Doe");
-        dataDictionary.Add("[Contact.Email]", "john@doe.com");
-        dataDictionary.Add("[Contact.PhoneNumber]", "1234567890");
-    }
-
     [Fact]
-    public void ReplacePlaceholdersShoulReplaceValues()
+    public void ReplacePlaceholdersShouldReplaceValues()
     {
         // Arrange
+        string value = "Cerere 1 Alocare Credentiale Pentru Plata Impozitelor Si Taxelor Locale Pentru Persoane Fizice\n\nSubsemnatul/a [Person.FirstName] [Person.LastName], e-mail [Contact.Email], numar de\ntelefon [Contact.PhoneNumber], solicit a-mi fi atribuit credential in\nvederea platii prin www.ghiseul.ro";
+        Dictionary<string, string> dataDictionary = new()
+        {
+            { "[Person.FirstName]", "John" },
+            { "[Person.LastName]", "Doe" },
+            { "[Contact.Email]", "john@doe.com" },
+            { "[Contact.PhoneNumber]", "1234567890" }
+        };
 
         // Act
         string result = TextUtilities.ReplacePlaceholders(value, dataDictionary);
@@ -32,6 +28,14 @@ public class TextUtilitiesTests
     public void ReplacePlaceholdersShouldNotContainSquareBrackets()
     {
         // Arrange
+        string value = "Cerere 1 Alocare Credentiale Pentru Plata Impozitelor Si Taxelor Locale Pentru Persoane Fizice\n\nSubsemnatul/a [Person.FirstName] [Person.LastName], e-mail [Contact.Email], numar de\ntelefon [Contact.PhoneNumber], solicit a-mi fi atribuit credential in\nvederea platii prin www.ghiseul.ro";
+        Dictionary<string, string> dataDictionary = new()
+        {
+            { "[Person.FirstName]", "John" },
+            { "[Person.LastName]", "Doe" },
+            { "[Contact.Email]", "john@doe.com" },
+            { "[Contact.PhoneNumber]", "1234567890" }
+        };
 
         // Act
         string result = TextUtilities.ReplacePlaceholders(value, dataDictionary);
