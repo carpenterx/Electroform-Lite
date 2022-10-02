e31c57000ef4fa2837afdad6cd8bb055d59bb48b
Sorin Iordache <jordache_sor@yahoo.com>
2022-10-02 21:48:09 +0300

    Add alias delete on document delete

--- ElectroformLite.Domain/Models/Alias.cs
+++ ElectroformLite.Domain/Models/Alias.cs
@@ -3,11 +3,11 @@
 public class Alias
 {
     public Guid Id { get; set; }
-
     public Guid AliasTemplateId { get; set; }
-
     public Guid DataGroupId { get; set; }
+    public Guid DocumentId { get; set; }
 
     public AliasTemplate AliasTemplate { get; set; }
     public DataGroup DataGroup { get; set; }
+    public Document Document { get; set; }
 }
